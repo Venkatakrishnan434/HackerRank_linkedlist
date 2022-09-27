63351459775fa4602be6d68164fc5d460ccc570e
Venkatakrishnan <venkatakrishnan434@gmail.com>
2022-09-27 13:03:40 +0530

    Delete node

--- delete_node.rb
+++ delete_node.rb
@@ -1,6 +1,9 @@
+
+# Complete the deleteNode function in the editor below.
+
 def deleteNode(llist, position)
     llist.delete_at(position)
     print llist
-
 end
+
   deleteNode([20, 6, 2, 19, 7, 4, 15, 9], 3)
